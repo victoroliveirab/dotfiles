0456b27063e7312c8acad7f63e1a34cde35b5e89
Victor Oliveira <eu@victoroliveira.com.br>
2023-11-14 18:02:04 -0300

    adds qmk config

---
+++ keyboard/victoroliveirab/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Mouse Speed set to "constant"
+#define MK_3_SPEED
+#define MK_C_OFFSET_0 1
+#define MK_C_INTERVAL_0 32
+#define MK_C_OFFSET_1 4
+#define MK_C_INTERVAL_1 16
+#define MK_C_OFFSET_2 16
+#define MK_C_INTERVAL_2 16
