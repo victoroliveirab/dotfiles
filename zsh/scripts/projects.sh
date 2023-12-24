09705e7f582efd8d53f5e6fa088c11d709c70a7b
Victor Oliveira <eu@victoroliveira.com.br>
2023-12-24 12:48:43 -0300

    adds zsh files for mac

---
+++ zsh/scripts/projects.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+FOLDER_NAME="workspaces"
+PROJECTS_ROOT="$HOME/$FOLDER_NAME"
+
+PROJECT=$(find $PROJECTS_ROOT -maxdepth 5 -path "*.git" | \
+  sed -e "s|$PROJECTS_ROOT/||g" | \
+  sed -e "s|/.git||g" | \
+  fzf)
+
+if [[ -n "$PROJECT" ]]; then
+  PROJECT_FULL_PATH="$PROJECTS_ROOT/$PROJECT"
+  clear
+  cd "$PROJECT_FULL_PATH"
+fi
