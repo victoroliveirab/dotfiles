3aef35bd6e28b445131241c3d76ebc8767577101
Victor Oliveira <victor.barbosa@hotelengine.com>
2024-05-28 12:09:35 -0300

    update dotfiles from work pc

--- nvim/lua/victor/plugins/conform.lua
+++ nvim/lua/victor/plugins/conform.lua
@@ -1,3 +1,25 @@
+local biome_or_prettier = function(bufnr)
+  local has_prettier = vim.fs.find({
+    -- https://prettier.io/docs/en/configuration.html
+    ".prettierrc",
+    ".prettierrc.json",
+    ".prettierrc.yml",
+    ".prettierrc.yaml",
+    ".prettierrc.json5",
+    ".prettierrc.js",
+    ".prettierrc.cjs",
+    ".prettierrc.toml",
+    "prettier.config.js",
+    "prettier.config.cjs",
+  }, { upward = true })[1]
+
+  if has_prettier then
+    return { "prettier" }
+  end
+
+  return { "biome" }
+end
+
 return {
   "stevearc/conform.nvim",
   event = { "BufReadPre", "BufNewFile" },
@@ -6,10 +28,10 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
+        javascript = biome_or_prettier,
+        typescript = biome_or_prettier,
+        javascriptreact = biome_or_prettier,
+        typescriptreact = biome_or_prettier,
         svelte = { "prettier" },
         css = { "prettier" },
         html = { "prettier" },
