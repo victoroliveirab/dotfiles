de3e80fd63617def898a2c9dad4a4ba48c3d1f59
Victor Oliveira <victor.barbosa@hotelengine.com>
2024-09-12 20:56:58 -0300

    update from professional

--- nvim/lua/victor/plugins/telescope.lua
+++ nvim/lua/victor/plugins/telescope.lua
@@ -62,4 +62,39 @@ return {
 
 		end,
 	},
+  {
+    "axkirillov/easypick.nvim",
+    event = "VeryLazy",
+    dependencies = {  
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function() 
+      local get_default_branch = "git rev-parse --symbolic-full-name refs/remotes/origin/HEAD | sed 's!.*/!!'"
+      local base_branch = vim.fn.system(get_default_branch) or "main"
+      local easypick = require("easypick")
+      easypick.setup({
+        pickers = {
+          -- diff current branch with base_branch and show files that changed with respective diffs in preview
+          {
+            name = "changed_files",
+            command = "git diff --name-only $(git merge-base HEAD " .. base_branch .. " )",
+            previewer = easypick.previewers.branch_diff({base_branch = base_branch})
+          },
+          -- list files that have conflicts with diffs in preview
+          {
+            name = "conflicts",
+            command = "git diff --name-only --diff-filter=U --relative",
+            previewer = easypick.previewers.file_diff()
+          },
+	      }
+      })
+
+      vim.keymap.set("n", "<leader>hf", "<cmd>Easypick changed_files<cr>", {
+        desc = "Open Git Changed Files",
+      })
+      vim.keymap.set("n", "<leader>hc", "<cmd>Easypick conflicts<cr>", {
+        desc = "Open Git Conflicted Files",
+      })
+    end,
+  }
 }
